c1e065efe9b531ad5b3bc89a7662e636bf260f46
acostyle <acostyle.m.z@protonmail.com>
2021-10-11 18:52:51 +0600

    chore(foodcartapp): add inlines to OrderedProduct and register Order model

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -3,6 +3,8 @@ from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
 
+from .models import Order
+from .models import OrderedProduct
 from .models import Product
 from .models import ProductCategory
 from .models import Restaurant
@@ -12,6 +14,17 @@ from .models import RestaurantMenuItem
 class RestaurantMenuItemInline(admin.TabularInline):
     model = RestaurantMenuItem
     extra = 0
+
+
+class OrderedProductInline(admin.TabularInline):
+    model = OrderedProduct
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderedProductInline,
+    ]
 
 
 @admin.register(Restaurant)
