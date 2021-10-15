146776de2fe689dd8e42e8552766872baa39183e
acostyle <acostyle.m.z@protonmail.com>
2021-10-15 22:23:54 +0600

    feat(foodcartapp): change related_name in OrderedProduct fields

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -10,50 +10,14 @@ class OrderQuerySet(models.QuerySet):
     def count_order_cost(self):
         return self.annotate(total_cost=
             Sum(
-                F('order__product__price') * F('order__quantity'),
+                F('ordered_products__product__price') * F('ordered_products__quantity'),
             )
         )
 
 
-class Order(models.Model):
-    PROCESSED = 'processed'
-    UNPROCESSED = 'unprocessed'
-    ORDER_STATUS_CHOICES = [
-        (PROCESSED, 'Обработанный'),
-        (UNPROCESSED, 'Необработанный'),
-    ]
-
-    CASH = 'cash'
-    CARD = 'card'
-    ORDER_PAYMENT_METHOD_CHOICES = [
-        (CASH, 'Наличные'),
-        (CARD, 'Карта')
-    ]
-
-    firstname = models.CharField(max_length=64, verbose_name='имя')
-    lastname = models.CharField(max_length=64, verbose_name='фамилия')
-    phonenumber = PhoneNumberField(verbose_name='номер телефона')
-    address = models.CharField(max_length=256, verbose_name='адрес доставки')
-    status = models.CharField(max_length=12, choices=ORDER_STATUS_CHOICES, default=UNPROCESSED, verbose_name='статус заказа')
-    commentary = models.CharField(max_length=256, blank=True, verbose_name='комментарий')
-    registrated_at = models.DateTimeField(default=timezone.now, verbose_name='дата регистрации')
-    called_at = models.DateTimeField(null=True, blank=True, verbose_name='дата звонка')
-    delivered_at = models.DateTimeField(null=True, blank=True, verbose_name='дата доставки')
-    payment_method = models.CharField(max_length=4, choices=ORDER_PAYMENT_METHOD_CHOICES, default=CASH, verbose_name='способ оплаты')
-
-    objects = OrderQuerySet.as_manager()
-
-    class Meta:
-        verbose_name = 'заказ клиента'
-        verbose_name_plural = 'заказы клиентов'
-
-    def __str__(self):
-        return f'{self.firstname} {self.address}'
-
-
 class OrderedProduct(models.Model):
-    product = models.ForeignKey('Product', related_name='product', on_delete=models.CASCADE, verbose_name='продукт')
-    order = models.ForeignKey('Order', related_name='order', on_delete=models.CASCADE, verbose_name='заказ')
+    product = models.ForeignKey('Product', related_name='ordered_products', on_delete=models.CASCADE, verbose_name='продукт')
+    order = models.ForeignKey('Order', related_name='ordered_products', on_delete=models.CASCADE, verbose_name='заказ')
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)], verbose_name='количество')
     cost = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, validators=[MinValueValidator(0)], verbose_name='Стоимость')
 
@@ -184,3 +148,50 @@ class RestaurantMenuItem(models.Model):
 
     def __str__(self):
         return f"{self.restaurant.name} - {self.product.name}"
+
+
+class Order(models.Model):
+    PROCESSED = 'processed'
+    UNPROCESSED = 'unprocessed'
+    ORDER_STATUS_CHOICES = [
+        (PROCESSED, 'Обработанный'),
+        (UNPROCESSED, 'Необработанный'),
+    ]
+
+    CASH = 'cash'
+    CARD = 'card'
+    ORDER_PAYMENT_METHOD_CHOICES = [
+        (CASH, 'Наличные'),
+        (CARD, 'Карта')
+    ]
+
+    firstname = models.CharField(max_length=64, verbose_name='имя')
+    lastname = models.CharField(max_length=64, verbose_name='фамилия')
+    phonenumber = PhoneNumberField(verbose_name='номер телефона')
+    address = models.CharField(max_length=256, verbose_name='адрес доставки')
+    
+    status = models.CharField(max_length=12, choices=ORDER_STATUS_CHOICES, default=UNPROCESSED, verbose_name='статус заказа')
+    commentary = models.CharField(max_length=256, blank=True, verbose_name='комментарий')
+    payment_method = models.CharField(max_length=4, choices=ORDER_PAYMENT_METHOD_CHOICES, default=CASH, verbose_name='способ оплаты')
+    
+    registrated_at = models.DateTimeField(default=timezone.now, verbose_name='дата регистрации')
+    called_at = models.DateTimeField(null=True, blank=True, verbose_name='дата звонка')
+    delivered_at = models.DateTimeField(null=True, blank=True, verbose_name='дата доставки')
+
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.SET_NULL,
+        verbose_name='забрать в ресторане',
+        related_name='orders',
+        null=True,
+        blank=True,
+    )
+
+    objects = OrderQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = 'заказ клиента'
+        verbose_name_plural = 'заказы клиентов'
+
+    def __str__(self):
+        return f'{self.firstname} {self.address}'
