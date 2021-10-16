ba6002ea043866e6fc3258a891391df59a9bfea4
Medet <56687178+acostyle@users.noreply.github.com>
2021-10-16 15:15:53 +0600

    refactor: refactor code (#21)

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -16,49 +16,53 @@ from .models import RestaurantMenuItem
 
 def banners_list_api(request):
     # FIXME move data to db?
-    return JsonResponse([
-        {
-            'title': 'Burger',
-            'src': static('burger.jpg'),
-            'text': 'Tasty Burger at your door step',
+    return JsonResponse(
+        [
+            {
+                "title": "Burger",
+                "src": static("burger.jpg"),
+                "text": "Tasty Burger at your door step",
+            },
+            {
+                "title": "Spices",
+                "src": static("food.jpg"),
+                "text": "All Cuisines",
+            },
+            {
+                "title": "New York",
+                "src": static("tasty.jpg"),
+                "text": "Food is incomplete without a tasty dessert",
+            },
+        ],
+        safe=False,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
         },
-        {
-            'title': 'Spices',
-            'src': static('food.jpg'),
-            'text': 'All Cuisines',
-        },
-        {
-            'title': 'New York',
-            'src': static('tasty.jpg'),
-            'text': 'Food is incomplete without a tasty dessert',
-        }
-    ], safe=False, json_dumps_params={
-        'ensure_ascii': False,
-        'indent': 4,
-    })
+    )
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def product_list_api(request):
-    products = Product.objects.select_related('category').available()
+    products = Product.objects.select_related("category").available()
 
     dumped_products = []
     for product in products:
         dumped_product = {
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-            'special_status': product.special_status,
-            'description': product.description,
-            'category': {
-                'id': product.category.id,
-                'name': product.category.name,
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "special_status": product.special_status,
+            "description": product.description,
+            "category": {
+                "id": product.category.id,
+                "name": product.category.name,
             },
-            'image': product.image.url,
-            'restaurant': {
-                'id': product.id,
-                'name': product.name,
-            }
+            "image": product.image.url,
+            "restaurant": {
+                "id": product.id,
+                "name": product.name,
+            },
         }
         dumped_products.append(dumped_product)
     return Response(dumped_products)
@@ -67,7 +71,7 @@ def product_list_api(request):
 class OrderedProductSerializer(ModelSerializer):
     class Meta:
         model = OrderedProduct
-        fields = ['id', 'product', 'quantity']
+        fields = ["id", "product", "quantity"]
 
 
 class OrderSerializer(ModelSerializer):
@@ -75,27 +79,30 @@ class OrderSerializer(ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['id', 'products', 'firstname', 'lastname', 'phonenumber', 'address']
+        fields = ["id", "products", "firstname", "lastname", "phonenumber", "address"]
 
 
 @transaction.atomic
-@api_view(['POST'])
+@api_view(["POST"])
 def register_order(request):
     serializer = OrderSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    
+
     order = Order.objects.create(
-        firstname=serializer.validated_data['firstname'],
-        lastname=serializer.validated_data['lastname'],
-        phonenumber=serializer.validated_data['phonenumber'],
-        address=serializer.validated_data['address']
+        firstname=serializer.validated_data["firstname"],
+        lastname=serializer.validated_data["lastname"],
+        phonenumber=serializer.validated_data["phonenumber"],
+        address=serializer.validated_data["address"],
     )
 
-    product_fields = serializer.validated_data['products']
-    OrderedProduct.objects.bulk_create([OrderedProduct(order=order, **fields) for fields in product_fields])
+    product_fields = serializer.validated_data["products"]
+    OrderedProduct.objects.bulk_create(
+        [OrderedProduct(order=order, **fields) for fields in product_fields]
+    )
     order_serializer = OrderSerializer(order)
 
     return Response(order_serializer.data)
+
 
 def get_place(address):
     place, is_created = Place.objects.get_or_create(address=address)
@@ -111,19 +118,21 @@ def get_place(address):
 
 
 def get_available_restaurants_with_products(order):
-    products_in_restaurants = RestaurantMenuItem.objects.prefetch_related('restaurant', 'product').filter(availability=True)
+    products_in_restaurants = RestaurantMenuItem.objects.prefetch_related(
+        "restaurant", "product"
+    ).filter(availability=True)
     order_products = order.ordered_products.all()
 
     restaurants_with_needed_products = []
     for order_product in order_products:
         for product_in_restaurants in products_in_restaurants:
             if product_in_restaurants.product == order_product.product:
-                restaurants_with_needed_products.append(product_in_restaurants.restaurant)
-    
-    return calculate_distances_to_order(
-        restaurants_with_needed_products,
-        order.address
-    )
+                restaurants_with_needed_products.append(
+                    product_in_restaurants.restaurant
+                )
+
+    return calculate_distances_to_order(restaurants_with_needed_products, order.address)
+
 
 def calculate_distances_to_order(restaurants, order_address):
     order_place = get_place(order_address)
@@ -131,13 +140,16 @@ def calculate_distances_to_order(restaurants, order_address):
     for restaurant in restaurants:
         restaurant_place = get_place(restaurant.address)
         distance_between_restauraunt_and_order = distance.distance(
-            (order_place.lat, order_place.lon), (restaurant_place.lat, restaurant_place.lon),
+            (order_place.lat, order_place.lon),
+            (restaurant_place.lat, restaurant_place.lon),
         ).km
         restaurants_with_order_distance.append(
             {
-                'restaurant': restaurant,
-                'order_distance': round(distance_between_restauraunt_and_order, 2),
+                "restaurant": restaurant,
+                "order_distance": round(distance_between_restauraunt_and_order, 2),
             }
         )
-    return sorted(restaurants_with_order_distance, key=lambda restaurant: restaurant['order_distance'])
-    
+    return sorted(
+        restaurants_with_order_distance,
+        key=lambda restaurant: restaurant["order_distance"],
+    )
--- places/models.py
+++ places/models.py
@@ -7,30 +7,37 @@ from star_burger import settings
 
 
 class Place(models.Model):
-    address = models.CharField(max_length=256, unique=True, verbose_name='адресс доставки')
-    lat = models.FloatField(blank=True, null=True, verbose_name='широта')
-    lon = models.FloatField(blank=True, null=True, verbose_name='долгота')
-    created_at = models.DateTimeField(default=timezone.now, verbose_name='дата создания')
-
+    address = models.CharField(
+        max_length=256, unique=True, verbose_name="адрес доставки"
+    )
+    lat = models.FloatField(blank=True, null=True, verbose_name="широта")
+    lon = models.FloatField(blank=True, null=True, verbose_name="долгота")
+    created_at = models.DateTimeField(
+        default=timezone.now, verbose_name="дата создания"
+    )
 
     def __str__(self):
         return self.address
 
-
     def fetch_coordinates(address):
         base_url = "https://geocode-maps.yandex.ru/1.x"
-        response = requests.get(base_url, params={
-            "geocode": address,
-            "apikey": settings.YA_API_KEY,
-            "format": "json",
-        })
+        response = requests.get(
+            base_url,
+            params={
+                "geocode": address,
+                "apikey": settings.YA_API_KEY,
+                "format": "json",
+            },
+        )
         response.raise_for_status()
-        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+        found_places = response.json()["response"]["GeoObjectCollection"][
+            "featureMember"
+        ]
 
         if not found_places:
             return None
 
         most_relevant = found_places[0]
-        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+        lon, lat = most_relevant["GeoObject"]["Point"]["pos"].split(" ")
 
         return lon, lat
