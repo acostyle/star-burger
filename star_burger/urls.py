66a702b74fdb2e48d72fe76be23bf81eb20cc306
acostyle <acostyle.m.z@protonmail.com>
2021-11-14 16:01:52 +0600

    chore: add debug_toolbar

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -29,4 +29,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path(r'__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
